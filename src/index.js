289e73be0ebab64faa261c36a73f6d6366649a1b
tr1ten <shubhi.blog@gmail.com>
2024-03-13 13:13:02 +0530

    sigin target

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-class App extends Component {
- render() {
-   return (
-     <div>This is a React component inside of Webflow!</div>
-  )
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Sign In
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
-}
+
 
 ReactDOM.render(
 React.createElement(App, {}, null),
-document.getElementById('react-target')
+document.getElementById('react-target-signin')
 );
 
